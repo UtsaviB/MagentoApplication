d8de1d4b53d1d3909d3f79af2fd1c7a852da3d64
utsavi <utsavi@hp-PC>
2018-12-22 15:17:16 +0530

    Third commit

--- TestNGProject/src/com/abc/magento/MagentoTest.java
+++ TestNGProject/src/com/abc/magento/MagentoTest.java
@@ -23,7 +23,7 @@ public class MagentoTest {
 	@Test
 	public void Test() throws Exception
 	{
-		String url="http://www.google.com";
+		String url="http://www.facebook.com";
 		driver.get(url);
 		
 		driver.findElement(By.id("email")).sendKeys("utsavi443@gmail.com");
